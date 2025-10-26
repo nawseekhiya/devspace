dee9cf2ac4a0dc889d5e2285f86c985a8b0f87b7
nawseekhiya <araj80317@gmail.com>
2025-10-27 04:29:30 +0530

    chore: init astro project, add tailwind, image, sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import image from '@astrojs/image';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [image(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
